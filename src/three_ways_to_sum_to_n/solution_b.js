d520a9ba13f6c26ab895da2d23273bb4c91298c2
lucyshuai <lucyshuai@hotmail.com>
2019-09-19 21:00:32 +0800

    second solution to three_ways_to_sum_to_n

--- src/three_ways_to_sum_to_n/solution_b.js
+++ src/three_ways_to_sum_to_n/solution_b.js
@@ -3,6 +3,8 @@
 // Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
-var sum_to_n = function(n) {
-    // your code here
+var sum_to_n = function (n) {
+    // your code here // your code here
+    let sum = (n + 1) / 2 * (n + 0);
+    return sum;
 };
